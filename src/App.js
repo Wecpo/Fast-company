6bc4049f3a0bcbcafff84705cfb9b1089ca7527a
Maksim Senin <wecpo@yandex.ru>
2022-11-26 11:20:21 +0300

    added pagination and prop-types

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import React, { useState } from "react";
-import Users from "./components/users";
-import SearchStatus from "./components/searchStatus";
-import api from "./api";
+import Users from "./app/components/users";
+import SearchStatus from "./app/components/searchStatus";
+import api from "./app/api";
 
 function App() {
-  const [users, setUsers] = useState(api.users.fetchAll());
-
-  const handleDelete = (userId) => {
-    setUsers((prevState) => prevState.filter((item) => item._id !== userId));
-  };
-
-  const handleToggleBookMark = (id) => {
-    setUsers((prevState) =>
-      prevState.map((item) => {
-        if (item._id === id) {
-          item.bookmark ? (item.bookmark = false) : (item.bookmark = true);
-        }
-
-        return item;
-      })
+    const [users, setUsers] = useState(api.users.fetchAll());
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
+    };
+    const handleToggleBookMark = (id) => {
+        setUsers(
+            users.map((user) => {
+                if (user._id === id) {
+                    return { ...user, bookmark: !user.bookmark };
+                }
+                return user;
+            })
+        );
+    };
+    return (
+        <div>
+            <SearchStatus length={users.length} />
+            <Users
+                onDelete={handleDelete}
+                onToggleBookMark={handleToggleBookMark}
+                users={users}
+            />
+        </div>
     );
-  };
-  return (
-    <div>
-      <SearchStatus length={users.length} />
-
-      <Users
-        users={users}
-        onDelete={handleDelete}
-        onToggleBookMark={handleToggleBookMark}
-      />
-    </div>
-  );
 }
 
 export default App;
