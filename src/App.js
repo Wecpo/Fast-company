c7021ce91cbd364227abd784c40f6da796242323
Maksim Senin <wecpo@yandex.ru>
2022-12-17 19:19:30 +0300

    added links for each users

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-import Users from "./app/components/users";
+import Users from "./app/layouts/users";
+import NavBar from "./app/components/navBar";
+import Login from "./app/layouts/login";
+import Main from "./app/layouts/main";
+import { Switch, Route } from "react-router-dom";
+import UserList from "./app/components/userList";
 
 function App() {
-    return <Users />;
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route path="/users/:userId" exact component={UserList} />
+                <Route path="/login" component={Login} />
+                <Route path="/users" component={Users} />
+                <Route path="/" component={Main} />
+            </Switch>
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>
+);
