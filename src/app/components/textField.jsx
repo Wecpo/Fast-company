b7c6ddc96b0b84e6d8636dcc8a4a91c3c08217f2
Maksim Senin <wecpo@yandex.ru>
2023-01-09 20:04:34 +0300

    added search string

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const TextField = ({ label, type, name, value, onChange, error }) => {
+const TextField = ({
+    label,
+    type,
+    name,
+    value,
+    onChange,
+    error,
+    placeholder
+}) => {
     const [showPassword, setShowPassword] = useState(false);
     const getInputClasses = () => {
         return "form-control" + (error ? " is-invalid" : "");
@@ -20,6 +28,7 @@ const TextField = ({ label, type, name, value, onChange, error }) => {
                     name={name}
                     value={value}
                     onChange={onChange}
+                    placeholder={placeholder}
                 />
                 {type === "password" && (
                     <button
@@ -40,7 +49,8 @@ const TextField = ({ label, type, name, value, onChange, error }) => {
     );
 };
 TextField.defaultProps = {
-    type: "text"
+    type: "text",
+    placeholder: ""
 };
 TextField.propTypes = {
     label: PropTypes.string,
@@ -48,7 +58,8 @@ TextField.propTypes = {
     name: PropTypes.string,
     value: PropTypes.string,
     onChange: PropTypes.func,
-    error: PropTypes.string
+    error: PropTypes.string,
+    placeholder: PropTypes.string
 };
 
 export default TextField;
