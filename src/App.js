3c4de656dbc54b75c03bbfd17a4890d1aa88d102
Maksim Senin <wecpo@yandex.ru>
2023-02-13 18:21:38 +0300

    before hw-14

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import NavBar from "./app/components/ui/navBar";
 import Login from "./app/layouts/login";
 import Main from "./app/layouts/main";
 import UserEditPage from "./app/components/page/userEditPage/userEditPage";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { ProfessionProvider } from "./app/hooks/useProfession";
 
 function App() {
     return (
-        <>
+        <div>
             <NavBar />
             <Switch>
-                <Route path="/users/:userId?/edit" exact component={UserEditPage} />
-                <Route path="/users/:userId?" exact component={Users} />
-                <Route path="/login/:type?" component={Login} />
-                <Route path="/users" component={Users} />
+                <ProfessionProvider>
+                    <Route
+                        path="/users/:userId?/edit"
+                        exact
+                        component={UserEditPage}
+                    />
+                    <Route path="/users/:userId?" exact component={Users} />
+                    <Route path="/login/:type?" component={Login} />
+                </ProfessionProvider>
                 <Route path="/" component={Main} />
+                <Redirect to="/" />
             </Switch>
-        </>
+            <ToastContainer />
+        </div>
     );
 }
 
--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -3,20 +3,23 @@ import { useParams } from "react-router-dom";
 import UserEditPage from "../components/page/userEditPage";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
+import UserProvider from "../hooks/useUsers";
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
     return (
         <>
-            {userId ? (
-                edit ? (
-                    <UserEditPage />
+            <UserProvider>
+                {userId ? (
+                    edit ? (
+                        <UserEditPage />
+                    ) : (
+                        <UserPage userId={userId} />
+                    )
                 ) : (
-                    <UserPage userId={userId} />
-                )
-            ) : (
-                <UsersListPage />
-            )}
+                    <UsersListPage />
+                )}
+            </UserProvider>
         </>
     );
 };
